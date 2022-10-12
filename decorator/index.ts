326dc6769dfd4a55d315ae1898180e5a6f4a587c
Brunno Lorenzoni <brunno.lorenzoni@unicred.com.br>
2022-10-12 08:50:02 -0300

    decorator

---
+++ decorator/index.ts
@@ -0,0 +1,23 @@
+import { Component } from "./components/Component";
+import { ConcreteComponent } from "./components/ConcreteComponent";
+import { ConcreteDecoratorA } from "./decorators/ConcreteDecoratorA";
+import { ConcreteDecoratorB } from "./decorators/ConcreteDecoratorB";
+
+function clientCode(component: Component) {
+  console.log(`RESULT: ${component.operation()}`);
+}
+
+const simple = new ConcreteComponent();
+console.log("Client: I've got a simple component");
+clientCode(simple);
+
+console.log('\n')
+
+const decorator1 = new ConcreteDecoratorA(simple);
+console.log('decorator1', decorator1.operation())
+
+const decorator2 = new ConcreteDecoratorB(decorator1);
+console.log('decorator2', decorator2.operation())
+
+console.log("Client: Now i've got a decorated component");
+clientCode(decorator2);
